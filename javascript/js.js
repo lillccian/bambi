5da346688c721f0aaf65f0e043597d4792cec949
eason.lin <eason.lin@phrozen3d.com>
2023-07-30 23:12:40 +0800

    first commit

---
+++ javascript/js.js
@@ -0,0 +1,301 @@
+let map_points = {
+      "1": {
+        point_name: "斑比山丘專屬免費停車場",
+        info: "離大門約108m下湖路&内湖一路交叉路口，<br>僅供停放車輛不負保管責任，<br>贵重物品請隨身攜帶.",
+      },
+      "2": {
+        point_name: "入門口",
+        info: "現場購票與線上預約的朋友<br>請到入口門報到準備進場購票。",
+      },
+      "3": {
+        point_name: "取號機",
+        info: "因實施入園人數控管，如遇入園人數已滿時，<br>現埸購票的朋友請到此抽入埸號碼牌<br>叫到號後再至入口門報到！",
+      },
+      "4": {
+        point_name: "拉布貨櫃廁所",
+        info: "背面畫有絨毛拉布圖樣<br>也是個拍照的好背景喔！",
+      },
+      "5": {
+        point_name: "立食區",
+        info: "園區內禁帶外食，可先在這裡吃完喔！<br>園內也設有外食寄放櫃。",
+      },
+      "6": {
+        point_name: "導覽教室",
+        info: "注意事項要仔細𦕲:喔！<br>＃只摸鹿的背部就好",
+      },
+      "7": {
+        point_name: "娃娃車停放區",
+        info: "為維護您的安全，輪椅及娃娃車禁止進入互動區<br>入固時建議使用嬰兒背帶，<br>此處僅供停放不負保管責任。不便之處，敬請見諒。",
+      },
+      "8": {
+        point_name: "手章/防蚊液/奇物櫃",
+        info: "•手章：蓋個手章、持當日票根，即可在當日營業時間內重複入園蔘觀唷！<br>•防蚊液：入園前請務必使用斑比山丘提供的有機精油防蚊液，避免小黑蚊叮咬後引起過敏反應。<br>•青物櫃：退幣式奇物櫃及外食奇放櫃，啊不要忘記把自己的東西帶回家唷。",
+      },
+      "9": {
+        point_name: "廁所",
+        info: "設有親子廁所。",
+      },
+      "10": {
+        point_name: "育嬰室",
+        info: "設有冷熱飲水機、溫奶器、洗手台、<br>酒精、哺乳椅、尿布台。",
+      },
+      "11": {
+        point_name: "無障礙廁所",
+        info: "設有無障礙廁所",
+      },
+      "12": {
+        point_name: "戶外休息區",
+        info: "走累了嗎～<br>在這裡休息一下來支美美子霜淇淋吧！",
+      },
+      "13": {
+        point_name: "愛心傘",
+        info: "互勒區是全戶外的.下雨天時忘記帶傘的朋友們請取用唷#請原鹿歸還<br>為維護您的安全請勿穿著雨衣，斑比們喜歡去咬輕飄飄的東西。",
+      },
+      "14": {
+        point_name: "吸煙區",
+        info: "園區內除了吸區以外<br>都是#全面禁的喔！",
+      },
+      "15": {
+        point_name: "售票櫃台",
+        info: "購票後請等候叫號，<br>入場聽導覽解說。",
+      },
+      "16": {
+        point_name: "斑比選物（紀念品區）",
+        info: "斑比山丘獨家設計的紀念品。<br>入園前先準備好拍照必備斑比造型髮箍啦！",
+      },
+      "17": {
+        point_name: "拉布的房間",
+        info: "出口處的拉布房間是必打卡場景，<br>拍完之後往前走就是紀念品店囉！<br>帶一個拉布娃娃陪你回家!#一鹿平安",
+      },
+      "18": {
+        point_name: "斑比森林入口",
+        info: "探險開始囉！<br>尋找斑比、水豚還有迷你驢姐妹的身影吧～",
+      },
+      "19": {
+        point_name: "洗鞋區",
+        info: "如果在斑比森林踩到不愉快的東西<br>可以來這裡沖一沖～",
+      },
+      "20": {
+        point_name: "斑比森林出口",
+        info: "探險結束了嗎?出口旁就是<span>(J)<span>斑比回憶，來斑比網美牆拍張紀念照吧！",
+      },
+      "21": {
+        point_name: "美美子咖啡廳",
+        info: "一邊喝咖啡、吃甜點<br>一邊看斑比與水豚在窗前跟你打招呼<br>還有招牌手煮焦糖乳酪布丁一定要來品嚐！",
+      },
+      "22": {
+        point_name: "美美子霜淇淋",
+        info: "一天供應兩種口味，使用嚴選原料且無添加香料<br>北海道十勝牛奶口味，絕對是入園必吃！",
+      },
+      "A": {
+        point_name: "桃花鹿區",
+        info: "兌換的體驗飼料中，有一沒愛心蘿蔔<br>可以到桃花平台搖鈴，呼喚屬於你的情鹿<br>記下咬走蘿蔔的是哪知情鹿<br>再到愛情小屋求得籤詩！",
+      },
+      "B": {
+        point_name: "情鹿籤詩",
+        info: "全台唯一，情鹿籤詩！快來找你的桃花鹿<br>看看情鹿大師怎麼解析你的愛情<br>#愛情這條鹿，帶你走向下一步！",
+      },
+      "C": {
+        point_name: "愛神拉布",
+        info: "獲得籤詩後，沈浸在粉紅泡泡中<br>快來找愛神拉布一起合照吧！<br><br>多摸摸斑比們讓你愛情滿分！",
+      },
+      "D": {
+        point_name: "水䐁",
+        info: "地瓜球與可樂餅在此出沒！<br>快來這裡捕捉牠們的身影吧～<br><br>水豚們只吃草<br>#請勿餵食紅蘿蔔及仙豆喔",
+      },
+      "E": {
+        point_name: "迷你驢",
+        info: "閒來沒事就喜歡高歌的妮妮＆娜娜<br>#請不要餵食喔！<br>她們只吃斑比保姆配比的營養餐<br><br>#切記請不要站在迷你驢後方<br>她們會調皮地踢你一腳喔！",
+      },
+      "F": {
+        point_name: "公鹿",
+        info: "斑比男孩們的繁殖季來臨時<br>為了保護他們避免因打架而導致彼此或遊客受傷，<br>就會讓他們在道一區休息唷。",
+      },
+      "G": {
+        point_name: "斑比小點心餐車",
+        info: "每張全票都附有一張體驗飼料兌換券<br>來小餐車找斑比夥伴兌換吧！<br>#小秘密：鹿仙豆能讓你成為人氣王<br>讓你更近距離與斑比們互動喔！",
+      },
+      "H": {
+        point_name: "斑比宿舍",
+        info: "噓...最高品質靜悄悄<br>斑比們休息時間正在睡午覺呢！<br><br>迷你驢妮妮＆娜娜因為毛髮不防水<br>只要遇上下雨天，也會待在宿舍喔",
+      },
+      "I": {
+        point_name: "斑比回憶",
+        info: "留下可以帶回家的斑比回憶！<br>不定期開放拍照活動，歡迎詢問現場工作人員",
+      },
+      "J": {
+        point_name: "斑比山丘的山丘",
+        info: "拍美照的好地方！<br>為維護草皮狀態，不定期開放，敬請見諒！",
+      },
+      "K": {
+        point_name: "落羽松區",
+        info: "一年四季落羽松不同的樣貌怎麼拍都唯美！",
+      },
+      "L": {
+        point_name: "斑比字母牆",
+        info: "不管站著拍、坐著拍，還是抱著拍都美～",
+      },
+      "M": {
+        point_name: "梅花鹿休息區",
+        info: "每天會輪班一些斑比在這區讓大家隔著欄杆做互動<br>小朋友呀、不太敢近距離靠近動物們的遊客<br>可以在這一區與他們做互動唷。",
+      },
+      "N": {
+        point_name: "小鹿",
+        info: "斑比山丘年紀最小萌寵!宜蘭、知名、景點就在這！因為小鹿還沒長大所以只能餵他們吃草唷！",
+      }
+    },
+    newsData;
+const apiURL = 'https://www.bambiland.com.tw/api/';
+
+function loadbar() {
+  var img = document.images,
+    c = 0,
+    tot = img.length;
+  if (tot == 0) return doneLoading();
+
+  function imgLoaded() {
+    c += 1;
+    var perc = ((100 / tot * c) << 0);
+    $(".percent").text(perc);
+    if (c === tot) return doneLoading();
+  }
+  function doneLoading() {
+    setTimeout(function() {
+      AOS.init();
+      // AOS.init({disable: 'mobile'});
+      $(".kv_logo").addClass("on");
+      $("#loading").fadeOut();
+      $("body").addClass("init");
+    },2000);
+  }
+  for (var i = 0; i < tot; i++) {
+    var tImg = new Image();
+    tImg.onload = imgLoaded;
+    tImg.onerror = imgLoaded;
+    tImg.src = img[i].src;
+  }
+}
+document.addEventListener('DOMContentLoaded', loadbar, false);
+$('.kvSlide').slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  arrows: false,
+  infinite: true,
+  swipe: true,
+  fade: true,
+  dots: true,
+  autoplay: true,
+  responsive: [
+    {
+      breakpoint: 767,
+      settings: {
+        slidesToShow: 1,
+        swipe: true,
+      }
+    }
+  ]
+});
+
+$('.animalSlide').slick({
+  centerMode: true,
+  centerPadding: '29%',
+  slidesToShow: 1,
+  infinite: false,
+  responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        centerMode: true,
+        centerPadding: '29%',
+        slidesToShow: 1
+      }
+    }
+  ]
+});
+
+$(".fixMenu_nav_left ul li a").on("click", function () {
+  $("input#menu").prop("checked", false);
+})
+
+function triggerPop(ta) {
+  if (ta === undefined) {
+    $("#animalsPop").fadeOut(300, function () {
+      $("#animalsPop .animalsPop_content").removeClass("on");
+    });
+  } else {
+    $("#animalsPop .animalsPop_content:eq(" + ta + ")").addClass("on");
+    $("#animalsPop").fadeIn();
+  }
+}
+
+function triggerNewsPop(ta, i, type) {
+  if (ta === undefined) {
+    $("#newsPop").fadeOut(300, function () {
+      $("body").addClass("init");
+      $("#newsPop .newsPop_content").hide();
+    });
+  } else {
+    $("#newsPop .newsPop_content:eq(" + ta + ")").show();
+    if(ta == 0){
+      let imgString = "";
+      console.log(i);
+      $.each(newsData[i].image,function(i2,el){
+        imgString += '<li><i></i><img src="'+el+'" alt=""></li>'
+      })
+      $(".newsPop_content .newsPop_content_box").html('<div class="contentL"><ul>'+imgString+'</ul></div><div class="contentR"><h4>'+newsData[i].master_title+'</h4><h6>'+newsData[i].master_subtitle+'</h6><p>'+newsData[i].master_content+'</p></div>');
+    }
+    if (ta == 3) {
+      $(".newsPop_content_map .contentL img").attr("src","images/map_photos/photo_"+i+".png");
+      $(".newsPop_content_map .contentR").attr("class", "contentR");
+      $(".newsPop_content_map .contentR").addClass("zone" + type);
+      $(".newsPop_content_map .contentR .num span").text(i);
+      $(".newsPop_content_map .contentR >h6").html(map_points[i].point_name);
+      $(".newsPop_content_map .contentR >p").html(map_points[i].info);
+    }
+    $("body").removeClass("init");
+    $("#newsPop").fadeIn();
+  }
+}
+function showZone(ta) {
+  if ($(".map >.hide").length && !$(".map ." + ta).hasClass("hide")) {
+    $(".map >.hide").removeClass("hide");
+    $(".map_tab >a.hide").removeClass("hide");
+  } else {
+    $(".map ." + ta).removeClass("hide").siblings().addClass("hide");
+    $(".map_tab a." + ta).removeClass("hide").siblings().addClass("hide");
+  }
+
+}
+
+function track(act, cat, label) {
+  gtag('event', cat, {
+    'event_category': act,
+    'event_label': label
+  });
+}
+
+$(window).on("scroll", function (e) {
+  if ($(window).scrollTop() > 200) {
+    $(".fixMenu_btn,header,#fixBtns").addClass("on");
+  } else {
+    $(".fixMenu_btn,header,#fixBtns").removeClass("on");
+  }
+})
+// document.addEventListener('aos:in', ({ detail }) => {
+//   let cls = $(detail).attr("id");
+//   console.log(cls);
+//   switch (cls){
+//     case "kv":
+//       $("#fixMenu_main").removeClass("on");
+//       break;
+//     case "news":
+//       $("#fixMenu_main").addClass("on");
+//       break;
+//   }
+// });
+function truncateString(str) {
+  if (str.length > 24) {
+    return str.slice(0, 24) + "...";
+  }
+  return str;
+}
